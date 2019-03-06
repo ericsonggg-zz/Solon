a09819f9ef752a690feb321b3d9b6f84ce0dba19
Eric Song <ericsongg@hotmail.com>
2019-03-06 16:04:22 -0500

    Add core screens

--- app/src/main/java/com/bme/solon/SplashActivity.java
+++ app/src/main/java/com/bme/solon/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.bme.solon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    public void toHome(View view) {
+        startActivity(new Intent(SplashActivity.this, HomeActivity.class));
     }
 }
