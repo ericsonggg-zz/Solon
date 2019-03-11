95f592974d28ed8e284595ef0fb75a45d9608644
Eric Song <ericsongg@hotmail.com>
2019-03-11 13:44:45 -0400

    Toolbar half done

--- app/src/main/java/com/bme/solon/HomeActivity.java
+++ app/src/main/java/com/bme/solon/HomeActivity.java
@@ -2,6 +2,7 @@ package com.bme.solon;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar appToolbar = (Toolbar) findViewById(R.id.app_toolbar);
+        setSupportActionBar(appToolbar);
     }
 }
