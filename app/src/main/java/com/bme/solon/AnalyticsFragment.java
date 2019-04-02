ff2bb91f69da2e8221556db9903254a8f3aac7ee
Eric Song <ericsongg@hotmail.com>
2019-04-02 00:28:10 -0400

    Renamed calendar in Analytics

--- app/src/main/java/com/bme/solon/AnalyticsFragment.java
+++ app/src/main/java/com/bme/solon/AnalyticsFragment.java
@@ -2,6 +2,7 @@ package com.bme.solon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,8 +34,22 @@ public class AnalyticsFragment extends MainFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_analytics, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_analytics, container, false);
+
+        //find Views
+        riskText = fragmentView.findViewById(R.id.analytics_risk_time_data);
+        riskSubText = fragmentView.findViewById(R.id.analytics_risk_time_subtext);
+        responseText = fragmentView.findViewById(R.id.analytics_response_time_data);
+        responseSubText = fragmentView.findViewById(R.id.analytics_response_time_subtext);
+        incidentText = fragmentView.findViewById(R.id.analytics_incident_count_data);
+        incidentSubText = fragmentView.findViewById(R.id.analytics_incident_count_subtext);
+        utiText = fragmentView.findViewById(R.id.analytics_uti_count_data);
+        utiSubText = fragmentView.findViewById(R.id.analytics_uti_count_subtext);
+        calendarView = fragmentView.findViewById(R.id.analytics_calendar);
+        
+        return fragmentView;
     }
 
     @Override
